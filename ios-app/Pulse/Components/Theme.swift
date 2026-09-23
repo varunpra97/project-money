154d20c16f930b1e92f3a0fe1325d01d2bbb4805
varunpra97 <varunprakashy1997@gmail.com>
2026-09-22 22:44:36 -0700

    iOS: light grey theme

--- ios-app/Pulse/Components/Theme.swift
+++ ios-app/Pulse/Components/Theme.swift
@@ -7,8 +7,8 @@ extension Color {
     static let pulseGreen = Color(red: 0.0, green: 200.0 / 255.0, blue: 5.0 / 255.0)
     /// Robinhood red
     static let pulseRed = Color(red: 1.0, green: 80.0 / 255.0, blue: 0.0)
-    static let pulseBg = Color.white
-    static let pulseCard = Color(white: 0.94)
+    static let pulseBg = Color(white: 0.9)
+    static let pulseCard = Color(white: 0.97)
     static let pulseSecondary = Color(white: 0.35)
     static let pulseTertiary = Color(white: 0.55)
 }
